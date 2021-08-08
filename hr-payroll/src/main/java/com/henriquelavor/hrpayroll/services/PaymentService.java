0ac5d56071370a518e43cdd92833b72daf070d4d
Henrique Lavor <henriquelavor@gmail.com>
2021-08-08 17:10:30 -0400

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/henriquelavor/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.henriquelavor.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.henriquelavor.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.00, days); //Payment Mocking
+	}
+}
