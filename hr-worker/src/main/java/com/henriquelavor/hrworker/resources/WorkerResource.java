63ef9ee67e95d32e497a2c6972240e2bd7cace56
Henrique Lavor <henriquelavor@gmail.com>
2021-08-14 17:55:04 -0400

    Ajust in resource hr-worker

--- hr-worker/src/main/java/com/henriquelavor/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/henriquelavor/hrworker/resources/WorkerResource.java
@@ -22,38 +22,95 @@ import com.henriquelavor.hrworker.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 	
-	private Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
+	/*
+	 * @Value("${test.config}") private String testConfig;
+	 */
 	
 	@Autowired
-	private Environment env; // esta variavel tem varios informacoes do contexto da aplicacao, como variaveis de configuracao etcc.
+	private Environment env;
 	
 	@Autowired
 	private WorkerRepository repository;
 	
-	//ResponseEntity encapsula resposta Http
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs() {
+		//logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}		
+	
 	@GetMapping
-	public ResponseEntity<List<Worker>> findAll(){
+	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
-	}
+	}	
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Worker> findById(@PathVariable Long id){
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
-	}
-	
-	
-	@GetMapping(value = "/configs")
-	public ResponseEntity<Void> getConfigs(){
-		logger.info("CONFIG =" + testConfig);
-		return ResponseEntity.noContent().build(); //para ser uma resposta sem corpo
-	}
-
+	}	
 }
+
+/*
+ * import java.util.List;
+ * 
+ * import org.slf4j.Logger; import org.slf4j.LoggerFactory; import
+ * org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.beans.factory.annotation.Value; import
+ * org.springframework.cloud.context.config.annotation.RefreshScope; import
+ * org.springframework.core.env.Environment; import
+ * org.springframework.http.ResponseEntity; import
+ * org.springframework.web.bind.annotation.GetMapping; import
+ * org.springframework.web.bind.annotation.PathVariable; import
+ * org.springframework.web.bind.annotation.RequestMapping; import
+ * org.springframework.web.bind.annotation.RestController;
+ * 
+ * import com.henriquelavor.hrworker.entities.Worker; import
+ * com.henriquelavor.hrworker.repositories.WorkerRepository;
+ * 
+ * @RefreshScope
+ * 
+ * @RestController
+ * 
+ * @RequestMapping(value = "/workers") public class WorkerResource {
+ * 
+ * private Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+ * 
+ * @Value("${test.config}") private String testConfig;
+ * 
+ * @Autowired private Environment env; // esta variavel tem varios informacoes
+ * do contexto da aplicacao, como variaveis de configuracao etcc.
+ * 
+ * @Autowired private WorkerRepository repository;
+ * 
+ * @GetMapping(value = "/configs") public ResponseEntity<Void> getConfigs(){
+ * logger.info("CONFIG =" + testConfig); return
+ * ResponseEntity.noContent().build(); //para ser uma resposta sem corpo }
+ * 
+ * //ResponseEntity encapsula resposta Http
+ * 
+ * @GetMapping public ResponseEntity<List<Worker>> findAll(){ List<Worker> list
+ * = repository.findAll(); return ResponseEntity.ok(list); }
+ * 
+ * @GetMapping(value = "/{id}") public ResponseEntity<Worker>
+ * findById(@PathVariable Long id){
+ * 
+ * logger.info("PORT = " + env.getProperty("local.server.port"));
+ * 
+ * Worker obj = repository.findById(id).get(); return ResponseEntity.ok(obj); }
+ * 
+ * 
+ * 
+ * }
+ */
